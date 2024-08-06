8b7ddbd6e23a6b3fe7519a1fc327ac399ff6b745
Amogh <amogh.maheshwari@email.com>
2024-08-06 09:09:18 -0700

    Server.js is completed

---
+++ server.js
@@ -0,0 +1,54 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const session = require('express-session');
+const path = require("path");
+
+const authController = require('./controllers/auth.js');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+// app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// new code above this line
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passUserToView);
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    user: req.session.user,
+  });
+});
+
+app.use('/auth', authController);
+app.use(isSignedIn);
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
