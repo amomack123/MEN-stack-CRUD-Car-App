5c2567db9d1bf95d8b0a498ce71ba7b9567d444a
Amogh <amogh.maheshwari@email.com>
2024-08-12 13:30:10 -0700

    fixed heroku error

--- controllers/cars.js
+++ controllers/cars.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user.js');
+
 const car = require('../models/car.js');
 
 ///users/:userId/cares
